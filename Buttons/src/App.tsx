fc90a3c7a1e9e4ff2af6dc3a9f7dfd8b2bb7e410
kelvi <kelvi_ugo@hotmail.com>
2022-07-02 17:34:23 -0300

    feat: cria projeto Buttons

---
+++ Buttons/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div className="container">Esta rodando na porta 8080</div>;
+ReactDOM.render(<App />, document.getElementById("app"));
