f8ea54b25d8ff7ad9e4ad9275d68c021b7f29e39
kelvi <kelvi_ugo@hotmail.com>
2022-07-02 17:36:55 -0300

    feat: cria o componente Buttons, no projeto 'Buttons'

---
+++ Buttons/src/components/Buttons.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+//@ts-ignore
+import useCountStore from "Container/stores/useCountStore";
+
+export default function Buttons() {
+  const increment = useCountStore((state) => state.increment);
+  const decrement = useCountStore((state) => state.decrement);
+  return (
+    <div style={{ display: "flex", gap: "0.8rem", marginTop: "2rem" }}>
+      <button
+        onClick={decrement}
+        style={{
+          width: "3rem",
+          padding: "0.4rem",
+          fontSize: "2rem",
+          color: "#fff",
+          backgroundColor: "#339345",
+          border: 0,
+          borderRadius: "50%",
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={increment}
+        style={{
+          width: "3rem",
+          padding: "0.4rem",
+          fontSize: "2rem",
+          color: "#fff",
+          backgroundColor: "#339345",
+          border: 0,
+          borderRadius: "50%",
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
