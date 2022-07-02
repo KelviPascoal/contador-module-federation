a3c0b084cd4f4550cb81dbfe9e6fae8c7af7bde6
kelvi <kelvi_ugo@hotmail.com>
2022-07-02 17:42:40 -0300

    refactor: corrige importação do Card na pagina home

--- Container/src/pages/Home.tsx
+++ Container/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Buttons } from "../comnponents/ButtonsRemote";
 //@ts-ignore
 import useCountStore from "Container/stores/useCountStore";
 import { Display } from "../comnponents/DisplayRemote";
-import { Card } from "../comnponents/Card";
+import { Card } from "../comnponents/CardRemote";
 
 export function Home() {
   const value = useCountStore((state) => state.value);
@@ -12,7 +12,7 @@ export function Home() {
     <>
       <p>{value}</p>
       <Card>
-        <h1 style={{ color: "#ededed" }}>Contador</h1>
+        <h1 style={{ color: "#f9f9f9" }}>Contador</h1>
         <Display />
 
         <Buttons />
